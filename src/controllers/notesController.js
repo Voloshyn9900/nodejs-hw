12328826d0d1022c88fd3f42fa986cabc7c4adf6
Voloshyn9900 <voloshyn9900@gmail.com>
2026-03-24 22:59:10 +0200

    Commit end

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,4 +1,4 @@
-// import createHttpError from 'http-errors';
+import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
@@ -6,10 +6,43 @@ export const getAllNotes = async (req, res) => {
   res.status(200).json(notes);
 };
 
-export const getNoteByIdController = async (req, res) => {};
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
 
-export const createNoteController = async (req, res) => {};
+  if (!note) {
+    throw createHttpError(404, 'Note not found!');
+  }
 
-export const deleteNoteController = async (req, res) => {};
+  res.status(200).json(note);
+};
 
-export const updateNoteController = async (req, res) => {};
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({ _id: noteId });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findByIdAndUpdate({ _id: noteId }, req.body, {
+    returnDocument: 'after',
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 import {
   getAllNotes,
-  getNoteByIdController,
-  createNoteController,
-  deleteNoteController,
-  updateNoteController,
+  getNoteById,
+  createNote,
+  deleteNote,
+  updateNote,
 } from '../controllers/notesController.js';
 
 const router = Router();
 
 router.get('/notes', getAllNotes);
-router.get('/notes/:noteId', getNoteByIdController);
-router.post('/notes', createNoteController);
-router.delete('/notes/:noteId', deleteNoteController);
-router.patch('/notes/:noteId', updateNoteController);
+router.get('/notes/:noteId', getNoteById);
+router.post('/notes', createNote);
+router.delete('/notes/:noteId', deleteNote);
+router.patch('/notes/:noteId', updateNote);
 
 export default router;
